1858d2a21193ed964f0661366809e3d5d584c523
uldynia <85220330+uldynia@users.noreply.github.com>
2023-06-15 16:19:37 +0800

    vibration? and arch

--- VRBootcampTemplate/Assets/Corn/CornPickup.cs
+++ VRBootcampTemplate/Assets/Corn/CornPickup.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.XR.PXR;
 
 
 [RequireComponent(typeof(AudioSource))]
@@ -9,6 +10,10 @@ public class CornPickup : MonoBehaviour
 {
     public PlayerStats stats;
     AudioSource audioData;
+    
+    public int vibrationDuration = 1000;
+    public float vibrationIntensity = 1.0f;
+
     private void Start()
     {
         audioData = GetComponent<AudioSource>();
@@ -17,6 +22,8 @@ public class CornPickup : MonoBehaviour
     {
         stats.cornsCollected++;
         audioData.Play();
+        PXR_Input.SendHapticImpulse(PXR_Input.VibrateType.BothController,
+            vibrationIntensity, vibrationDuration);
         Destroy(other.gameObject);
     }
 }
