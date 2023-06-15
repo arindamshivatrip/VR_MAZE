2c2ccc4fd0b801923fa80fdeeb09c54f3cfccec7
uldynia <85220330+uldynia@users.noreply.github.com>
2023-06-15 11:51:58 +0800

    water

--- VRBootcampTemplate/Assets/Corn/CornPickup.cs
+++ VRBootcampTemplate/Assets/Corn/CornPickup.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class CornPickup : MonoBehaviour
 {
+
+    AudioSource audioData;
+    private void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
     private void OnCollisionEnter(UnityEngine.Collision collision)
     {
+        
+        audioData.Play(0);
         Destroy(gameObject);
     }
 }
