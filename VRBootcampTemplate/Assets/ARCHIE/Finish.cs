ad58617547fbbc6d99f3106e055b44403fb830b1
uldynia <85220330+uldynia@users.noreply.github.com>
2023-06-15 16:57:26 +0800

    s

--- VRBootcampTemplate/Assets/ARCHIE/Finish.cs
+++ VRBootcampTemplate/Assets/ARCHIE/Finish.cs
@@ -6,23 +6,27 @@ public class Finish : MonoBehaviour
 {
     AudioSource audioData;
     public UnityEngine.UI.Text timer;
-    float timeElapsed = -5f;
+    float timeElapsed = 0f;
     bool won = false;
+    public int vibrationDuration = 10000;
+    public float vibrationIntensity = 1.0f;
     private void Start()
     {
         audioData = GetComponent<AudioSource>();
     }
     private void Update()
     {
-        if(won)
+        timer.text = "You took " + timeElapsed.ToString() + " seconds!";
+        if(!won)
         {
 
-            timer.text = "You took " + timeElapsed.ToString() + " seconds!";
+            timeElapsed += Time.deltaTime;
         }
     }
     private void OnTriggerEnter(Collider other)
     {
-        timeElapsed += Time.deltaTime;
+        Unity.XR.PXR.PXR_Input.SendHapticImpulse(Unity.XR.PXR.PXR_Input.VibrateType.BothController,
+                vibrationIntensity, vibrationDuration);
         won = true;
         audioData.Play();
     }
